445e64d25a89a78617823cd7c1e4251b18809856
Caren Thomas <carenthomas@fb.com>
2016-08-08 11:26:49 -0700

    add new APIs from internal fb303.thrift file
    Summary: open source additional APIs from the internal Facebook fb303.thrift file
    
    Test Plan: none
    
    Reviewers: simpkins
    
    Subscribers:
    
    Tasks:
    
    Blame Revision:

--- if/fb303.thrift
+++ if/fb303.thrift
@@ -59,9 +59,45 @@ service FacebookService {
   map<string, i64> getCounters(),
 
   /**
+   * Gets a subset of counters which match a
+   * Perl Compatible Regular Expression for this service
+   */
+  map<string, i64> getRegexCounters(1: string regex),
+
+  /**
+   * Get counter values for a specific list of keys.  Returns a map from
+   * key to counter value; if a requested counter doesn't exist, it won't
+   * be in the returned map.
+   */
+  map<string, i64> getSelectedCounters(1: list<string> keys),
+
+  /**
    * Gets the value of a single counter
    */
   i64 getCounter(1: string key),
+
+  /**
+   * Gets the exported string values for this service
+   */
+  map<string, string> getExportedValues(),
+
+  /**
+   * Get exported strings for a specific list of keys.  Returns a map from
+   * key to string value; if a requested key doesn't exist, it won't
+   * be in the returned map.
+   */
+  map<string, string> getSelectedExportedValues(1: list<string> keys),
+
+  /**
+   * Gets a subset of exported values which match a
+   * Perl Compatible Regular Expression for this service
+   */
+  map<string, string> getRegexExportedValues(1: string regex),
+
+  /**
+   * Gets the value of a single exported string
+   */
+  string getExportedValue(1: string key),
 
   /**
    * Sets an option
@@ -77,6 +113,22 @@ service FacebookService {
    * Gets all options
    */
   map<string, string> getOptions(),
+
+  /**
+   * Returns the 1-minute load average on the system (i.e. the load may not
+   * all be coming from the current process).
+   */
+  double getLoad(),
+
+  /**
+   * Returns the pid of the process
+   */
+  i64 getPid(),
+
+  /**
+   * Returns the command line used to execute this process.
+   */
+  string getCommandLine(),
 
   /**
    * Returns the unix time that the server has been running since
