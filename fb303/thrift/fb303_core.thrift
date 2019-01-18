08990eff398b1d3531026c3348c4e1a0a2476149
Adam Simpkins <simpkins@fb.com>
2019-01-17 17:26:08 -0800

    add a new fb303_core.thrift file
    Summary:
    Add a new nearly-empty open source fb303 thrift file.  In a future diff I plan
    move most of the existing fb303 FacebookService definition to the new
    BaseService defined in this file, and change FacebookService to extend
    BaseService.  Creating fb303_core.thrift as a nearly-empty file in this diff
    is primarily to make it easier to roll out these changes across our various
    code bases that use fb303.thrift.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D13641454
    
    fbshipit-source-id: 5f727ee6debdcd4b760f5c5c216064ffff95d2ed

---
+++ fb303/thrift/fb303_core.thrift
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace java com.facebook.fb303.core
+namespace java.swift com.facebook.swift.fb303.core
+namespace cpp fb303
+namespace d fb303
+namespace py.asyncio fb303_asyncio.fb303_core
+namespace py.twisted fb303_twisted.fb303_core
+namespace perl fb303
+namespace php fb303
+namespace node_module fb303
+namespace go fb303.thrift.fb303_core
+
+/**
+ * Standard base service interface.
+ *
+ * This interface provides methods to get some service metadata that is common
+ * across many services.
+ */
+service BaseService {
+
+}
