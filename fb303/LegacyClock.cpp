f699e464258213b1a9e7043d2d04a3a976a43b2d
Chad Austin <chadaustin@fb.com>
2019-05-22 15:31:15 -0700

    break ServiceData dependency on common/time
    Summary:
    To open source ServiceData and its dependencies, the dependency on common/time's WallClockUtil::NowInSecFast must be broken. Fortunately, NowInSecFast is equivalent to `std::time`, so introduce a new function that wraps `std::time` (and the faster CLOCK_GETTIME on macOS).
    
    Eventually, we plan to make fb303 and the stats code consistently use std::chrono, but that's a bigger change across all of fbcode.
    
    Reviewed By: yfeldblum, simpkins
    
    Differential Revision: D15226138
    
    fbshipit-source-id: c1bfa0ea827cec2bd3afcc11cd6e023facc15965

---
+++ fb303/LegacyClock.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <fb303/LegacyClock.h>
+
+#include <stdexcept>
+
+#include <folly/lang/Exception.h>
+
+namespace facebook {
+namespace fb303 {
+
+time_t get_legacy_stats_time() {
+#ifdef __APPLE__
+  timespec ts;
+  auto ret = clock_gettime(CLOCK_REALTIME, &ts);
+  if (folly::kIsDebug && (ret != 0)) {
+    folly::throw_exception<std::runtime_error>("Error using CLOCK_REALTIME.");
+  }
+  return ts.tv_sec;
+#else
+  return ::time(nullptr);
+#endif
+}
+
+} // namespace fb303
+} // namespace facebook
