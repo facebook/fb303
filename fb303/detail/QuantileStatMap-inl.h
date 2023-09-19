b4f39f93a57f14b947cee3cfee08e6ac0fee9c71
Giuseppe Ottaviano <ott@fb.com>
2023-09-19 06:55:06 -0700

    Revert D49392138: ($17,158.92/yr) Prevent expensive copy in fb303/detail/QuantileStatMap-inl.h
    Differential Revision:
    D49392138
    
    Original commit changeset: 52bb1d4b30e3
    
    Original Phabricator Diff: D49392138
    
    fbshipit-source-id: 9cecc4ad111c2bbbed01905c4c486782d1d98f0b

--- fb303/detail/QuantileStatMap-inl.h
+++ fb303/detail/QuantileStatMap-inl.h
@@ -92,7 +92,7 @@ void BasicQuantileStatMap<ClockT>::getValues(
         quantiles.push_back(statDef.quantile);
       }
     }
-    const auto& estimates = sme.stat->getEstimates(quantiles, now);
+    auto estimates = sme.stat->getEstimates(quantiles, now);
     auto timeSinceCreation = std::chrono::duration_cast<std::chrono::seconds>(
         now - sme.stat->creationTime());
     for (const auto& statDef : sme.statDefs) {
