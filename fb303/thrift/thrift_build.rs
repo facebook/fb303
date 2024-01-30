0a968ca5bd53404c662e4f5070d80aa799277ba3
David Tolnay <dtolnay@meta.com>
2024-01-29 19:35:06 -0800

    Move server traits to a separate crate
    Summary: This diff turns a `thrift_library` with `name = "foo"` into 4 Rust targets: `:foo-rust` (as before), `:foo-rust-types` (as before), `:foo-rust-clients` (as before), and `:foo-rust-services` (new).
    
    Reviewed By: zertosh, shayne-fletcher
    
    Differential Revision: D53166618
    
    fbshipit-source-id: 6f94f7a70f70de928f7320b06e6d638cc6ac1206

--- fb303/thrift/thrift_build.rs
+++ fb303/thrift/thrift_build.rs
@@ -45,6 +45,7 @@ fn main() {
 
         conf.types_crate("fb303_core__types");
         conf.clients_crate("fb303_core__clients");
+        conf.services_crate("fb303_core__services");
 
         let options = "deprecated_default_enum_min_i32,serde";
         if !options.is_empty() {
