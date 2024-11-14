1f8341fbc861c3edde8de39025627d834480067f
Joel Jones <joelejones@meta.com>
2024-11-14 11:45:07 -0800

    prevent TLStatsAsyncAggregator from saving RequestContext to AsyncTimeout (#62)
    Summary:
    
    Currently, TLStatsAsyncAggregator::scheduleAggregation will call AsyncTimeout::scheduleTimeout which saves as a default parameter the current RequestContext. The possible issue with this is that RequestContext can possibly persist indefinitely until program end since on the timer expiration, the class calls scheduleTimeout, this cycle will continue.
    
    This issue has happened in other areas too, such as HHWheelTimer. Since this class's counters operate on a global scope, it does not make sense to carry any request specific information into the timeout. Thus, for the invocations of scheduleTimeout, we pass in nullptr for the RequestContext.
    
    The test case now shows that the RequestContext object is no longer persisted into the timeout.
    
    Differential Revision: D65911229

--- fb303/TLStatsAsyncAggregator.cpp
+++ fb303/TLStatsAsyncAggregator.cpp
@@ -40,12 +40,16 @@ void TLStatsAsyncAggregator::scheduleAggregation(
   // that can generate stats.
   attachEventBase(eventBase, folly::AsyncTimeout::InternalEnum::INTERNAL);
   intervalMS_ = intervalMS;
-  scheduleTimeout(intervalMS_);
+
+  // Pass in nullptr for RequestContext so it is not persisted.
+  // These counters are global and should not carry request specific
+  // context.
+  scheduleTimeout(intervalMS_, nullptr);
 }
 
 void TLStatsAsyncAggregator::timeoutExpired() noexcept {
   stats_->aggregate();
-  scheduleTimeout(intervalMS_);
+  scheduleTimeout(intervalMS_, nullptr);
 }
 
 } // namespace facebook::fb303
