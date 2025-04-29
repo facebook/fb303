c4bd3b44e166635098d41a7a7a1db3bc6931444d
Dan Deng <danieldeng@meta.com>
2025-04-29 00:17:41 -0700

    Use fb303::TFunctionStatHandler in SidecarThriftServer (attempt 2)
    Summary: Issue with D67438345 was that `AsyncProcessorFactoryWrapper::getProcessor()` is called once per connection resulting in one instance of `TFunctionStatHandler` per connection. Each instance has it's own thread which becomes an issue for IG where the sidecar is shared with many separate processes.
    
    Reviewed By: prakashgayasen
    
    Differential Revision: D72949167
    
    fbshipit-source-id: 8fe838f4a2550c960572ecec02249a5e37f756dd

--- fb303/TFunctionStatHandler.h
+++ fb303/TFunctionStatHandler.h
@@ -410,6 +410,9 @@ class TFunctionStatHandler
       const folly::exception_wrapper& ew_) final;
 };
 
+std::shared_ptr<TFunctionStatHandler> createDefaultStatHandler(
+    const char* serviceName);
+
 /**
  * Enable Thrift handler call counters
  */
